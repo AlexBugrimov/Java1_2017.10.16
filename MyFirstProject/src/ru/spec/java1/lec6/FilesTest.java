724f787dad640871803219307089e1d0f07de161
student <student@tg6-14>
2017-10-24 20:05:37 +0300

    Доработка класса по работе с разбором документа

--- MyFirstProject/src/ru/spec/java1/lec6/FilesTest.java
+++ MyFirstProject/src/ru/spec/java1/lec6/FilesTest.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -36,18 +37,62 @@ public class FilesTest {
 					Integer count = stat.get(word);
 					count = count == null ? 1 : count + 1;
 					stat.put(word, count);
+					addStringToStat(word, 2);
 				}
 			}
 
 		}
 		// stat.forEach((k, v) -> System.out.println(k + " - " + v));
 
-		Set<Entry<String, Integer>> entrySet = stat.entrySet();
+		Set<Entry<String, Integer>> entrySet = bigrams.entrySet();
 		TreeSet<Entry<String, Integer>> sorted = new TreeSet<>((e1, e2) -> {
-										int cmp = e1.getValue().compareTo(e2.getValue());
-										return (-1) * (cmp == 0 ? e1.getKey().compareTo(e2.getKey()) : cmp);
-										});
+			int cmp = e1.getValue().compareTo(e2.getValue());
+			return (-1) * (cmp == 0 ? e1.getKey().compareTo(e2.getKey()) : cmp);
+		});
 		sorted.addAll(entrySet);
 		sorted.forEach(e -> System.out.println(e.getValue() + "\t" + e.getKey()));
+
+		Scanner scan = new Scanner(System.in);
+		for (; scan.hasNextLine();) {
+			String line = scan.nextLine();
+			System.out.println(check(line, 2));
+		}
+	}
+
+	static HashMap<String, Integer> bigrams = new HashMap<>();
+
+	static void addStringToStat(String src, int gram) {
+		src = src.toLowerCase().replaceAll("[^а-яё]+", " ").trim();
+		String[] words = src.split(" ");
+		for (String word : words) {
+			addWordToStat(word, gram);
+		}
+	}
+
+	static void addWordToStat(String word, int gram) {
+		int maxLen = word.length() - gram;
+		if (maxLen <= 0) {
+			iterator(word);
+		} else {
+			for (int i = 0; i < maxLen; i++) {
+				String ngram = word.substring(i, i + gram);
+				iterator(ngram);
+			}
+		}
+	}
+
+	private static void iterator(String word) {
+		bigrams.put(word, 1 + Optional.ofNullable(bigrams.get(word)).orElse(0));
+	}
+
+	static public boolean check(String src, int gram) {
+		src = src.toLowerCase().replaceAll("[^а-яё]+", " ").trim();
+		int maxLen = src.length() - gram;
+		for (int i = 0; i < maxLen; i++) {
+			if (bigrams.get(src.substring(i, i + gram)) == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
